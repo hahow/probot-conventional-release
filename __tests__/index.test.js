e624b1cc68712fad3604d9cc59c0df8afe393070
Amo Wu <amo260@gmail.com>
2017-12-25 16:37:26 +0800

    test: 補上 ConventionalCommit scope 的斜線測試
    原本的 conventionalCommitsParser 碰到 scope 有斜線會是 null，根據我們的使用需求修改規則。

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,25 @@
 // You can import your modules
 // const index = require('../index')
+const convertToConventionalCommit = require('../lib/convertToConventionalCommit')
 
-test('that we can run tests', () => {
+test('ConventionalCommit 的 scope 應該正確解析斜線（例如：controllers/auth）', () => {
+  const fakeResponse = {
+    commit: {
+      message: 'fix(controllers/auth): oauth login failed'
+    }
+  }
+
+  const {
+    conventionalCommit: {
+      type,
+      scope,
+      subject
+    }
+  } = convertToConventionalCommit(fakeResponse)
+
+  expect(type).toBe('fix')
+  expect(scope).toBe('controllers/auth')
+  expect(subject).toBe('oauth login failed')
 })
 
 // For more information about testing with Jest see:
