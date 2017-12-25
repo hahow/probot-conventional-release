e5015b6b972973fcd2ea0ba906507ac166dfa58e
Amo Wu <amo260@gmail.com>
2017-12-25 16:47:21 +0800

    refactor: export func for testing

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const conventionalCommitsParser = require('conventional-commits-parser')
 const handlebars = require('handlebars')
 const _ = require('lodash')
 const Rx = require('rx')
 const semver = require('semver')
+
+const convertToConventionalCommit = require('./lib/convertToConventionalCommit')
 
 /** 只有 push 到這個 branch 才會觸發 conventional release */
 const DEFAULT_BRANCH = 'master'
@@ -317,34 +318,6 @@ module.exports = (robot) => {
  */
 function concatAllCommits (acc, curr) {
   return acc.concat(curr.data)
-}
-
-/**
- * 使用 conventional-commits-parser 這個 package，
- * 將 GitHub getCommit API response 的 commit.message 轉成 conventional commit 的資料結構，
- * 並另外 assign commit.author 和 commit.sha 這兩個 field，因為之後 Release Notes 會需要這些資料
- *
- * @param {Object} response
- * @param {Object} response.commit
- * @param {Object} response.commit.author
- * @param {string} response.commit.author.name
- * @param {string} response.commit.message
- * @param {string} response.sha
- * @returns {Object} conventionalCommit
- * @returns {Object} conventionalCommit.author
- * @returns {string} conventionalCommit.author.name
- * @returns {Object} conventionalCommit.conventionalCommit
- * @returns {string} conventionalCommit.sha
- * @see https://github.com/conventional-changelog-archived-repos/conventional-commits-parser
- */
-function convertToConventionalCommit ({ commit: { author, message }, sha }) {
-  const conventionalCommit = conventionalCommitsParser.sync(message)
-
-  return {
-    conventionalCommit,
-    sha,
-    author
-  }
 }
 
 /**
