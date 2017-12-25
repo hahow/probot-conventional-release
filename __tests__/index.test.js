af1e01be4d28771c7c5da808b790c06796fc2eeb
Amo Wu <amo260@gmail.com>
2017-12-25 16:34:08 +0800

    test: remove demo code

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,8 +2,6 @@
 // const index = require('../index')
 
 test('that we can run tests', () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
 })
 
 // For more information about testing with Jest see:
